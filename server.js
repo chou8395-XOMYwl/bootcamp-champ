909eaf780b241e08a134ac6b67c45cd9cf6821a1
Mike L <mike.levy.rva@gmail.com>
2022-09-02 14:13:59 -0400

    login in page styles/ main.handlebars

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 const session = require('express-session');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
