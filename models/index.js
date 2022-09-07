71c0b5f8510846eacca7e908b86af9dddf1c1b26
Mike L <mike.levy.rva@gmail.com>
2022-09-07 15:27:49 -0400

    layout changes

--- models/index.js
+++ models/index.js
@@ -5,9 +5,9 @@ const Comment = require('./Comment');
 const Vote = require('./Vote')
 
 //create associations
-User.hasMany(Post, {
-    foreignKey: 'user_id'
-});
+// User.hasMany(Post, {
+//     foreignKey: 'user_id'
+// });
 
 Post.belongsTo(User, {
     foreignKey: 'user_id'
@@ -45,13 +45,14 @@ Comment.belongsTo(User, {
     foreignKey: 'user_id'
   });
 
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
+// Comment.belongsTo(Post, {
+//     foreignKey: 'post_id'
+// });
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
-});
+// User.hasMany(Comment, {
+//     foreignKey: 'user_id'
+// });
+
 
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
