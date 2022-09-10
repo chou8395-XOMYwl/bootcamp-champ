d35c134a6ef2d69c27e824aa28834be8816d0009
Mike L <mike.levy.rva@gmail.com>
2022-09-10 10:46:07 -0400

    heroku

--- server.js
+++ server.js
@@ -42,11 +42,11 @@ app.set('view engine', 'handlebars');
 
 
 // turn on connection to db and server
-// sequelize.sync({ force: false }).then(() => {
-//     app.listen(PORT, () => console.log('Now listening'));
-//   });
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
-});
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+//   sequelize.sync({ force: false });
+// });
