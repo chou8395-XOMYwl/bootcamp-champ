0a763cd35f2d49274b0e7a4130690b9490982be7
Vlah <brennan.vlahcevic@armssoftware.com>
2022-09-11 19:10:13 -0400

    table

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.set('view engine', 'handlebars');
 
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
 
