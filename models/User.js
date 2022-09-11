1dea13e3571244b6ab0fed75028f463c275989ad
Vlah <brennan.vlahcevic@armssoftware.com>
2022-09-11 15:53:25 -0400

    see

--- models/User.js
+++ models/User.js
@@ -38,16 +38,16 @@ class User extends Model {
         validate: {
           isEmail: true
         }
-      },
-      github: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        unique: true,
-      },
-      about: {
-        type: DataTypes.TEXT,
-        allowNull: true,
       }
+      // github: {
+      //   type: DataTypes.STRING,
+      //   allowNull: true,
+      //   unique: true,
+      // },
+      // about: {
+      //   type: DataTypes.TEXT,
+      //   allowNull: true,
+      // }
     },
     {
       hooks: {
